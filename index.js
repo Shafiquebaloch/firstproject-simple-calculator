c7dbc868ef86bd0c5dd4a59c99c493e15124587c
Shafiquebaloch <muhammadraheem4739733@gmail.com>
2024-03-21 23:53:28 +0500

    I Have added a new operator

--- index.js
+++ index.js
@@ -1,26 +1,28 @@
 #! /usr/bin/env node
 import inquirer from "inquirer";
 const answer = await inquirer.prompt([
-  { message: "Enter first number", type: "number", name: "firstNumber" },
-  { message: "Enter Second number", type: "number", name: "SecondNumber" },
-  {
-    message: "Select One of the Operators to perform Operation",
-    type: "list",
-    name: "operator",
-    choices: ["addition", "Substraction", "Multiply", "Divide", "Modulus"],
-  },
+    { message: "Enter first number", type: "number", name: "firstNumber" },
+    { message: "Enter Second number", type: "number", name: "SecondNumber" },
+    {
+        message: "Select One of the Operators to perform Operation",
+        type: "list",
+        name: "operator",
+        choices: ["addition", "Substraction", "Multiply", "Divide",],
+    },
 ]);
 console.log(answer);
 if (answer.operator === "addition") {
-  console.log(answer.firstNumber + answer.SecondNumber);
-} else if (answer.operator === "Substraction") {
-  console.log(answer.firstNumber - answer.SecondNumber);
-} else if (answer.operator === "Multiply") {
-  console.log(answer.firstNumber * answer.SecondNumber);
-} else if (answer.operator === "Divide") {
-  console.log(answer.firstNumber / answer.SecondNumber);
-} else if (answer.operator === "Modulus") {
-  console.log(answer.firstNumber % answer.SecondNumber);
-} else {
-  console.log("Sorry Choose a valid operator");
+    console.log(answer.firstNumber + answer.SecondNumber);
+}
+else if (answer.operator === "Substraction") {
+    console.log(answer.firstNumber - answer.SecondNumber);
+}
+else if (answer.operator === "Multiply") {
+    console.log(answer.firstNumber * answer.SecondNumber);
+}
+else if (answer.operator === "Divide") {
+    console.log(answer.firstNumber / answer.SecondNumber);
+}
+else {
+    console.log("Sorry Choose a valid operator");
 }
